a0cfe85145a3d0e63324e30f779b15a73cf07940
shaminshaju1504 <shaminshaju1504@gmail.com>
2025-02-13 19:54:36 -0500

    labssessment1

---
+++ scripts/app.js
@@ -0,0 +1,6 @@
+let studentFullName = 'Shamin shaju';
+let studentNumber="1203841";
+const result='studentFullName-$(studentNumber)';
+console.log(result);
+
+let headerCount=document.querySelector("h1");
